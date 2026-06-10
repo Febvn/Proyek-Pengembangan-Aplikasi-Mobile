e04836f892b1d981a80a89630c03b99ac2665b39
Febrian Valentino Nugroho <dodithalud01@gmail.com>
2026-06-10 21:18:35 +0700

    fix: resolve compilation error due to invalid LocalTextStyle import

--- app/src/main/java/com/itera/news/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/itera/news/presentation/screen/HomeScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.LocalTextStyle
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
